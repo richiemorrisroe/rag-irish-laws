2b47c7b734dc1431bebddd91b1ebda346eaaac27
Richard Morrisroe <richie.morrisroe@gmail.com>
2024-11-03 13:43:59 +0000

    update queries and move them

--- irish_statutes/utils.py
+++ irish_statutes/utils.py
@@ -16,7 +16,7 @@ def setup_logger(caller_file=None):
                         encoding='utf-8', level=logging.DEBUG)
 
     handler = logging.StreamHandler(sys.stdout)
-    handler.setLevel(logging.INFO)
+    handler.setLevel(logging.WARNING)
     formatter = logging.Formatter(
         '%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     handler.setFormatter(formatter)
