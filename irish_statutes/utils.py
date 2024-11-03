0043479aec61f5b16fbf7074f866df5d1b97c019
Richard Morrisroe <richie.morrisroe@gmail.com>
2024-11-03 13:16:32 +0000

    make logger work

--- irish_statutes/utils.py
+++ irish_statutes/utils.py
@@ -1,11 +1,18 @@
 import logging
+import re
 import sys
 
 
-def setup_logger():
+def setup_logger(caller_file=None):
+    if not caller_file:
+        logfile_name = f'{__file__}.log'
+    else:
+        filtered_caller_file = re.sub(r'\.py$', "", caller_file)
+        logfile_name = f'{filtered_caller_file}.log'
+        
     logger = logging.getLogger()
     
-    logging.basicConfig(filename=f'{__FILE__}.log',
+    logging.basicConfig(filename=logfile_name,
                         encoding='utf-8', level=logging.DEBUG)
 
     handler = logging.StreamHandler(sys.stdout)
