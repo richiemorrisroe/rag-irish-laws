6c600dad52cea03ac2296cd5b9211e1fd7453bab
Richard Morrisroe <richie.morrisroe@gmail.com>
2024-11-03 13:43:59 +0000

    update queries and move them

--- irish_statutes/law_query.py
+++ irish_statutes/law_query.py
@@ -25,6 +25,7 @@ from llama_index.core.query_engine import RetrieverQueryEngine
 from llama_index.core.postprocessor import SimilarityPostprocessor
 
 from utils import setup_logger
+from eval_queries import query_llm
 
 parser = argparse.ArgumentParser()
 
@@ -37,19 +38,9 @@ args = parser.parse_args()
 # DATA_DIR = './laws_test'
 
 logger = setup_logger(__file__)
-# logger = logging.getLogger()
 
-# logging.basicConfig(filename='law_query.log',
-#                     encoding='utf-8', level=logging.DEBUG)
 
-# handler = logging.StreamHandler(sys.stdout)
-# handler.setLevel(logging.WARNING)
-# formatter = logging.Formatter(
-#     '%(asctime)s - %(filename)s - %(name)s - %(levelname)s - %(message)s')
-# handler.setFormatter(formatter)
-# logger.addHandler(handler)
-
-# logging.warning("extracted all plain text")
+logger.warning("extracted all plain text")
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 logger.warning("downloaded embedding model")
 # ollama
@@ -110,46 +101,11 @@ if not args.query:
     query = "What are the requirements for data protection in ireland?"
 else:
     query = args.query
-query_engine = index.as_query_engine(similarity_top_k=2)
-prompts_dict = query_engine.get_prompts()
-logger.info(f"{prompts_dict=}")
+# query_engine = index.as_query_engine(similarity_top_k=2)
+# prompts_dict = query_engine.get_prompts()
+# logger.info(f"{prompts_dict=}")
 # display_prompt_dict(prompts_dict)
-response = query_engine.query(query)
-scores = [x.score for x in response.source_nodes]
-logger.warning(f"scores for matched documents were {scores=}")
-logger.info(f"{response.source_nodes}")
-
+response = query_llm(index, query)
+logger.warning(f"scores for matched documents were {response.scores=}")
 logger.info(f"{response.source_nodes=}")
 print(response)
-
-
-QUERIES = ["""What are the requirements for data protection under the 2018 act?""",
-           """What is the procedure for firing an employee in Irish law?""",
-           """what are the functions of a data protection officer?""",
-           """what information must motor insurance providers supply to policyholders?""",
-           """What constitutes a breach of equality law?""",
-           """How many breaks are employees entitled to during an eight hour shift?""",
-           """What are the legal requirements for an employment contract?""",
-           """What constitutes unfair dismissal under irish law?""",
-           """What grounds does the Equality act cover?""",
-           """Is it legal to import gasoline for personal use?""",
-           """What requirements must be met to transport gasoline?""",
-           """Under what grounds may a residential tenancy be terminated?""",
-           """May a joint residential tenancy be terminated if one of the parties leaves the property?""",
-           """What are the conditions required for irish citizenship?""",
-           """What are the conditions required for a claim of contructive dismissal to succeed?""",
-           """How is land ownership determined?""",
-           """What happens if someone dies without making a will?""",
-           """What is intestate succession in the context of inheritance?""",
-           """What is intestate succession?""",
-           """What is the minimum wage for full time employees?""",
-           """What is the national minimum hourly rate of pay?""",
-           """Is it legal to drive without insurance?"""
-           """What are the penalities for driving without insurance?""",
-           """Can i be prosecuted for allowing my friend to drive a vehicle on which they are not insured?""",
-           """What are the requirements for setting up a limited company in Ireland?""",
-           """What are the differences between a private limited company and a private unlimited company?""",
-           """Does a private limited company need to file accounts, and if so, how often?""",
-           
-           
-           ]
