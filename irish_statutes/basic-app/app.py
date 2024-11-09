8e0a78631ac2ae8d3997bf96a7695bf7d6d6c77f
Richard Morrisroe <richie.morrisroe@gmail.com>
2024-11-09 13:50:33 +0000

    add logging and shinyswatch, get results output working

--- irish_statutes/basic-app/app.py
+++ irish_statutes/basic-app/app.py
@@ -8,37 +8,37 @@ import pandas as pd
 
 from llama_index.core import Settings
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
-root_dir = Path(__file__).parent
-print(root_dir)
-sys.path.append(str(root_dir))
+# root_dir = Path(__file__).parent
+# print(root_dir)
+# sys.path.append(str(root_dir))
 
 from indexer.eval_queries import query_llm, setup_llm
 from indexer.vstore import get_index_from_database
+from indexer.utils import setup_logger
 
-from shiny import render, ui, reactive
-from shiny.express import input
-
-chat = ui.Chat(id='chat')
-
+from shiny import render, reactive
+from shiny.express import input, ui
+from shinyswatch import theme
 
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-base-en-v1.5")
 
 Settings.llm = setup_llm()
-
+logger = setup_logger("__FILE__")
 index = get_index_from_database()
-
-
-
 
 query = """Does a private limited company need to file accounts,
            and if so, how often?"""
-ui.panel_title("Lawbot")
+# ui.panel_title("Lawbot")
 ui.input_text("query", "Enter a query")
-ui.input_select(  
-    "select",  
-    "Is this a good answer?:",  
-    {"1A": "Yes", "1B": "No"},  
+ui.input_select(
+    "select",
+    "Is this a good answer?:",
+    {"1": "Yes", "0": "No", "NA": "Not yet rated"},
+    selected="Not yet rated"
 )
+ui.page_opts(title="Lawbot", theme=theme.sandstone)
+
+
 
 # @reactive.effect
 # def _():
@@ -46,35 +46,44 @@ ui.input_select(
 
 @render.text
 def query_txt():
-    sleep(1)
     return f"query is {input.query()}"
 
 
 results_dict = {}
 @render.ui
 def answer_query():
-    sleep(2)
     query_res = query_llm(index, input.query(), top_k=5)
-    
     resp = query_res.response
     source_nodes = query_res.source_nodes
-    print(len(source_nodes))
+    logger.info(f"{len(source_nodes)=}")
     node_text = [node.text for node in source_nodes]
     scores = query_res.scores
     scores_text = "Score2:".join(str(s) for s in scores)
     node_text_full = "\n\n".join(text for text in node_text)
     # top_k = query_res.top_k
-    results_dict[query] = [resp, source_nodes, scores]
+    # results_dict[query] = [resp, source_nodes, scores]
+    results_dict['query'] = query
+    results_dict['response'] = resp
+    results_dict['source_nodes'] = source_nodes
+    results_dict['scores'] = scores
+    logger.warning(f"{results_dict=}")
     return (ui.h1('Answer'), ui.markdown(resp),
             ui.h1('Scores'), ui.markdown(scores_text),
             ui.h1('Sources'), ui.markdown(node_text_full)
             )
 
-result_df = None
-if not result_df:
-    result_df = pd.DataFrame.from_dict(results_dict)
-else:
-    new_results = pd.DataFrame.from_dict(results_dict)
-    result_df = pd.concat([result_df, new_results])
 
-print(f"{result_df.head()=}")
+result_df = None
+
+
+@render.data_frame
+def add_to_results(result_df=result_df):
+    if not result_df:
+        results_dict["good_answer"] = input.select()
+        result_df = pd.DataFrame.from_dict(results_dict, orient='columns')
+    else:
+        results_dict["good_answer"] = input.select()
+        new_results = pd.DataFrame.from_dict(results_dict, orient='columns')
+        result_df = pd.concat([result_df, new_results])
+        logger.warning(f"{result_df.head()=}")
+    return result_df
